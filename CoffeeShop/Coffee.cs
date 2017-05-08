61e400029917125beb28b1ec15ae8af74c0b8839
Cris Silva Jr. <ishiikurisu@yahoo.com>
2017-05-08 11:06:08 -0300

    First setup running tests

---
+++ CoffeeShop/Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop
+{
+    public class Coffee
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public Coffee(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
---
+++ TestSuite/CoffeeTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using CoffeeShop;
+
+namespace TestSuite
+{
+    [TestFixture]
+    public class CoffeeTest
+    {
+        [Test]
+        public void TestarPrecoDoCafe()
+        {
+            Coffee coffee = new Coffee("black", 2.50);
+            Assert.AreEqual(2.50, coffee.Price);
+        }
+    }
+}
