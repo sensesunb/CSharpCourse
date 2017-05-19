2d6c660c890e034b35e544127592a1f60d65a006
Cris Silva Jr. <ishiikurisu@yahoo.com>
2017-05-19 11:19:34 -0300

    Basic coffee shop forms program done

--- CoffeeForms/Form1.Designer.cs
+++ CoffeeForms/Form1.Designer.cs
@@ -164,21 +164,25 @@
             // 
             // buttonAdd
             // 
+            this.buttonAdd.Enabled = false;
             this.buttonAdd.Location = new System.Drawing.Point(3, 3);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(75, 23);
             this.buttonAdd.TabIndex = 0;
             this.buttonAdd.Text = ">>";
             this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
             // buttonDelete
             // 
+            this.buttonDelete.Enabled = false;
             this.buttonDelete.Location = new System.Drawing.Point(3, 32);
             this.buttonDelete.Name = "buttonDelete";
             this.buttonDelete.Size = new System.Drawing.Size(75, 23);
             this.buttonDelete.TabIndex = 1;
             this.buttonDelete.Text = "<<";
             this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
             // 
             // buttonStart
             // 
@@ -200,6 +204,7 @@
             this.buttonOk.TabIndex = 1;
             this.buttonOk.Text = "Ok";
             this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
             // 
             // Form1
             // 
--- CoffeeForms/Form1.cs
+++ CoffeeForms/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeShop;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,17 +13,55 @@ namespace CoffeeForms
 {
     public partial class Form1 : Form
     {
+        private Cashier Cashier { get; set; }
+        private CoffeeShop.Menu Options { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+            Cashier = new Cashier();
+            Options = new CoffeeShop.Menu(@"C:\Users\cris\Documents\work\IEEE\CSharpCourse\dunkindonuts.txt");
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
-            // Load menu
-            // Start new order
-            // Disable start button
-            // Enable ok button
+            foreach (var description in Options.GetDescriptions()) {
+                listMenu.Items.Add(description);
+            }
+            Cashier.Start();
+            buttonStart.Enabled = false;
+            buttonOk.Enabled = buttonAdd.Enabled = buttonDelete.Enabled = true;
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = true;
+            buttonOk.Enabled = buttonAdd.Enabled = buttonDelete.Enabled = false;
+            listMenu.Items.Clear();
+            listOrder.Items.Clear();
+
+            Cashier.Finish();
+            MessageBox.Show(string.Format("{0:C}", Cashier.Total), "Compra Concluída! ☺");
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (listMenu.SelectedIndex >= 0)
+            {
+                var coffeeName = listMenu.SelectedItem as string;
+                Cashier.Add(Options.GetCoffee(coffeeName));
+                listOrder.Items.Add(coffeeName);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (listOrder.SelectedIndex >= 0)
+            {
+                int index = listOrder.SelectedIndex;
+                // TODO Implement removal of list items
+                listOrder.Items.RemoveAt(index);
+            }
         }
     }
 }
